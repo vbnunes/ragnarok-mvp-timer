1f09a302779b236f094d9846140347de7a3c997f
Vinicius Nunes <vnsbnunes@gmail.com>
2019-05-05 12:08:42 -0300

    Finish countdown funcionality and importing mvps gifs to the project

--- src/components/MPVItem/index.js
+++ src/components/MPVItem/index.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
 import './mvpitem.scss'
+import Countdown from 'react-countdown-now';
+import Notifier from "react-desktop-notification"
+
 
 export default class index extends Component {
+  state = { date: Date.now() + this.props.mvp.respawn_time };
+  
+  constructor(props) {
+    super(props);
+    this.api = React.createRef();
+    this.id_mvp = 1086;
+  }
+  
+  handleComplete = () => {
+    Notifier.focus("MVP Notification",`${this.props.mvp.name} just respawned`,"www.google.com","","MVP");
+  }
+
+  renderer = ({ hours, minutes, seconds, completed, api}) => {
+    if (completed) {
+      return <span>{("0" + hours).slice(-2)}:{("0" + minutes).slice(-2)}:{("0" + seconds).slice(-2)}</span>
+    } else {
+      this.api = api;
+      return <span>{("0" + hours).slice(-2)}:{("0" + minutes).slice(-2)}:{("0" + seconds).slice(-2)}</span>;
+    }
+  }
+
+  handleResetClick = () => { 
+    this.setState({ date: Date.now() + this.props.mvp.respawn_time});
+  };
   render() {
-    const {id, respawn_time, time_left, mvp_gif} = this.props.mvp;
+    const {id, map, respawn} = this.props.mvp;
 
     return (
         <div className="mvpitem-container">
           <div className="item-header">
-            <span className="respawn-time">{respawn_time}</span>
-            <span className="id-box">prt_sewb4</span>
+            <span className="respawn-time">{respawn}</span>
+            <span className="id-box">{map}</span>
           </div>
           <div className="image-box">
-            <img src={mvp_gif} alt={id}/>
+            <img src={`./img/${id}.gif`} alt={id}/>
           </div>
           <div className="timer-box">
-            00:00:00
+            <Countdown key={this.state.date} date={this.state.date} renderer={this.renderer} onComplete={this.handleComplete} autoStart={false} />
           </div>
           <div className="btn-container">
-            <button class="button green">Start</button>
-            <button class="button red">Pause</button> 
+            <button class="button green" onClick={() => this.api.start()}>Start</button>
+            <button class="button red" onClick={this.handleResetClick}> Reset</button> 
           </div>
       </div>
     )
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,49 +3,174 @@ import { combineReducers } from 'redux';
 const mvpReducer = () => {
     return [
         {
-            "id": 1,
-            "respawn_time": "1h20",
-            "time_left": 100,
-            "mvp_gif": "https://steamuserimages-a.akamaihd.net/ugc/102855490088437444/140E39F13BB6F39F539A196DD1E8BE5A4BEE88D4/"
+          "mvp_name": "GTB",
+          "id": 1086,
+          "map": "prt_sewb4",
+          "respawn": "1h",
+          "respawn_time": 3600000
         },
         {
-            "id": 1,
-            "respawn_time": "1h20",
-            "time_left": 100,
-            "mvp_gif": "https://www.ragnaplace.com/bro/mob/1147.gif"
+          "mvp_name": "Osiris",
+          "id": 1038,
+          "map": "moc_pryd04",
+          "respawn": "1h",
+          "respawn_time": 3600000
         },
         {
-            "id": 1,
-            "respawn_time": "1h20",
-            "time_left": 100,
-            "mvp_gif": "https://steamuserimages-a.akamaihd.net/ugc/102855490088437444/140E39F13BB6F39F539A196DD1E8BE5A4BEE88D4/"
+          "mvp_name": "AmonRa",
+          "id": 1511,
+          "map": "moc_pryd06",
+          "respawn": "1h",
+          "respawn_time": 3600000
         },
         {
-            "id": 1,
-            "respawn_time": "1h20",
-            "time_left": 100,
-            "mvp_gif": "https://steamuserimages-a.akamaihd.net/ugc/102855490088437444/140E39F13BB6F39F539A196DD1E8BE5A4BEE88D4/"
+          "mvp_name": "Dracula",
+          "id": 1389,
+          "map": "gef_dun01",
+          "respawn": "1h",
+          "respawn_time": 3600000
         },
         {
-            "id": 1,
-            "respawn_time": "1h20",
-            "time_left": 100,
-            "mvp_gif": "https://steamuserimages-a.akamaihd.net/ugc/102855490088437444/140E39F13BB6F39F539A196DD1E8BE5A4BEE88D4/"
+          "mvp_name": "Moonlight Flower",
+          "id": 1150,
+          "map": "pay_dun04",
+          "respawn": "1h",
+          "respawn_time": 3600000
         },
         {
-            "id": 1,
-            "respawn_time": "1h20",
-            "time_left": 100,
-            "mvp_gif": "https://steamuserimages-a.akamaihd.net/ugc/102855490088437444/140E39F13BB6F39F539A196DD1E8BE5A4BEE88D4/"
+          "mvp_name": "Turtle General",
+          "id": 1312,
+          "map": "tur_dun04",
+          "respawn": "1h",
+          "respawn_time": 3600000
         },
-
         {
-            "id": 1,
-            "respawn_time": "1h20",
-            "time_left": 100,
-            "mvp_gif": "https://steamuserimages-a.akamaihd.net/ugc/102855490088437444/140E39F13BB6F39F539A196DD1E8BE5A4BEE88D4/"
+          "mvp_name": "Nightmare AmonRa",
+          "id": 2362,
+          "map": "moc_prydn2",
+          "respawn": "1h",
+          "respawn_time": 3600000
         },
-    ]
+        {
+          "mvp_name": "Incantation Samurai",
+          "id": 1492,
+          "map": "ama_dun03",
+          "respawn": "1h31",
+          "respawn_time": 5460000
+        },
+        {
+          "mvp_name": "Bacsojin",
+          "id": 1630,
+          "map": "lou_dun03",
+          "respawn": "1h56",
+          "respawn_time": 6960000
+        },
+        {
+          "mvp_name": "Phreeoni",
+          "id": 1159,
+          "map": "moc_fild17",
+          "respawn": "2h",
+          "respawn_time": 7200000
+        },
+        {
+          "mvp_name": "Mistress",
+          "id": 1059,
+          "map": "mjolnir_04",
+          "respawn": "2h",
+          "respawn_time": 7200000
+        },
+        {
+          "mvp_name": "Boitata",
+          "id": 2068,
+          "map": "bra_dun02",
+          "respawn": "2h",
+          "respawn_time": 7200000
+        },
+        {
+          "mvp_name": "Kiel D-01",
+          "id": 1734,
+          "map": "kh_dun02",
+          "respawn": "2h",
+          "respawn_time": 7200000
+        },
+        {
+          "mvp_name": "Fallen Bishop Hibram",
+          "id": 1871,
+          "map": "abbey02",
+          "respawn": "2h",
+          "respawn_time": 7200000
+        },
+        {
+          "mvp_name": "Maya",
+          "id": 1147,
+          "map": "anthell02",
+          "respawn": "2h",
+          "respawn_time": 7200000
+        },
+        {
+          "mvp_name": "Eddga",
+          "id": 1115,
+          "map": "pay_fild10",
+          "respawn": "2h",
+          "respawn_time": 7200000
+        },
+        {
+          "mvp_name": "RSX-0806",
+          "id": 1623,
+          "map": "ein_dun02",
+          "respawn": "2h05",
+          "respawn_time": 7500000
+        },
+        {
+          "mvp_name": "Lord of the Dead",
+          "id": 1373,
+          "map": "Niflheim",
+          "respawn": "2h13",
+          "respawn_time": 7980000
+        },
+        {
+          "mvp_name": "Tao Gunka",
+          "id": 1583,
+          "map": "beach_dun",
+          "respawn": "5h",
+          "respawn_time": 18000000
+        },
+        {
+          "mvp_name": "Gloom Under the Night",
+          "id": 1768,
+          "map": "ra_san05",
+          "respawn": "5h",
+          "respawn_time": 18000000
+        },
+        {
+          "mvp_name": "Lady Tanee",
+          "id": 1688,
+          "map": "ayo_dun02",
+          "respawn": "7h",
+          "respawn_time": 25200000
+        },
+        {
+          "mvp_name": "Gioia",
+          "id": 2251,
+          "map": "gld2_ald",
+          "respawn": "8h",
+          "respawn_time": 28800000
+        },
+        {
+          "mvp_name": "Valkyrie Randgris",
+          "id": 1751,
+          "map": "odin_tem03",
+          "respawn": "8h",
+          "respawn_time": 28800000
+        },
+        {
+          "mvp_name": "Beelzebub",
+          "id": 1873,
+          "map": "abbey03",
+          "respawn": "12h",
+          "respawn_time": 43200000
+        }
+      ]
 }
 
 export default combineReducers({ 
